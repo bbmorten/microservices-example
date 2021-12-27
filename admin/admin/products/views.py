aecec3bdbe4ef51bb40e28022b7cbbf82a81c9ee
Bulent Morten <bbmorten@gmail.ccom>
2021-12-27 20:05:32 +0300

    End of Admin App

--- admin/admin/products/views.py
+++ admin/admin/products/views.py
@@ -1,8 +1,10 @@
 from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import status
+from rest_framework.views import APIView
+import random
 
-from .models import Product
+from .models import Product, User
 from .serializers import ProductSerializer
 
 class ProductViewSet(viewsets.ViewSet):
@@ -35,8 +37,15 @@ class ProductViewSet(viewsets.ViewSet):
         product = Product.objects.get(id=pk)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-        
+
 
         
+class UserAPIView(APIView):
+    def get(self, _):
+        users = User.objects.all()
+        user = random.choice(users)
+        return Response({'id': user.id})
 
+
+ 
 
